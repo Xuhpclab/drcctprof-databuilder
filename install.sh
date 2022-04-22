1b2df7ecb07a21fab3ec1699edf0af6783f0266e
dolanzhao <neuzqd@gmail.com>
2022-04-22 19:15:49 -0400

    add --user

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "install dependencies"
 PYTHON_BIN=$(which python3)
-${PYTHON_BIN} -m pip install --upgrade protobuf
+${PYTHON_BIN} -m pip install --user --upgrade protobuf
 git submodule init
 git submodule update
 cd hatchet
